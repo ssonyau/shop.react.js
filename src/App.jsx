b54f05082bf71b324602385e742391f0c4d877e4
Sonya Tkachenko <94766215+ssonyau@users.noreply.github.com>
2023-03-30 19:10:48 +0300

    basic settings and design

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./compinents/Header";
+import Footer from "./compinents/footer";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+
+export default App;
