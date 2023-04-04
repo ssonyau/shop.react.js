217a7eb9f0fae68ab0d44913fb83f191669600e3
Sonya Tkachenko <94766215+ssonyau@users.noreply.github.com>
2023-04-04 10:48:14 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,71 @@
+import React from "react";
 import Header from "./compinents/Header";
 import Footer from "./compinents/footer";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Gray chair',
+          img: 'chair-gray.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'table',
+          img: 'table.webp',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'tables',
+          price: '159.99'
+        },
+        {
+          id: 3,
+          title: 'dresser',
+          img: 'dresser.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'dressers',
+          price: '200.99'
+        },
+        {
+          id: 4,
+          title: 'wooden bed',
+          img: 'wooden-bed.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'beds',
+          price: '549.99'
+        },
+        {
+          id: 5,
+          title: 'iron bed',
+          img: 'iron bed.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'beds',
+          price: '999.99'
+        },
+        {
+          id: 5,
+          title: 'iron bed',
+          img: 'iron bed.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'beds',
+          price: '999.99'
+        },
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
 };
 
 
